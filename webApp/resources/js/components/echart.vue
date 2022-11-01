8e82147fca6067545eb8fd2a1179d6291dfa40a6
Nicolas  Martinez <niceduardo9@gmail.com>
2022-10-31 19:23:08 -0500

    Reestructuraccion Pagina Web

---
+++ webApp/resources/js/components/echart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="grafica" class="grafico" />
+</template>
+<script setup>
+import * as echarts from 'echarts';
+import { ref } from 'vue'
+
+const grafica = ref();
+
+function graficar(option) {
+  if (grafica.value !== null) {
+    const echart = echarts.init(grafica.value)
+    echart.setOption(option)
+  }
+}
+
+defineExpose({
+  graficar
+});
+</script>
+
+<style>
+.grafico {
+  width: 800px;
+  height: 500px;
+}
+</style>
